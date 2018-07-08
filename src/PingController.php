8bc474f95bb59a9fe02e24b714e9011d1438cda6
Lou117 <sylvain.glacon@icloud.com>
2018-07-08 18:22:27 +0200

    Uses Core ResponseFactory in PingController Updates composer.json to use lou117/core dev-master version in master branch

--- src/PingController.php
+++ src/PingController.php
@@ -8,22 +8,23 @@
 namespace Skeleton;
 
 use Lou117\Core\AbstractController;
-use Lou117\Core\Http\Response\JsonResponse;
+use Lou117\Core\ResponseFactory;
+use Psr\Http\Message\ResponseInterface;
 
 class PingController extends AbstractController
 {
     /**
      * Sends a JSON response with some basic information.
      *
-     * @return JsonResponse
+     * @return ResponseInterface
      */
-    public function ping(): JsonResponse
+    public function ping(): ResponseInterface
     {
-        $response = new JsonResponse([
+        $response = ResponseFactory::createJsonResponse([
             'text' => 'Welcome to Core framework v2!',
             'link' => 'https://github.com/lou117/core'
         ]);
 
-        return $response->addHeader('X-Core-Ack', time());
+        return $response->withHeader("X-Core-Ack", time());
     }
 }
